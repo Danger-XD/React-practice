4a36005292edf6873d03bcdb9394294b2ea0f157
Syed Mahmoud Sobhani <84802517+Danger-XD@users.noreply.github.com>
2024-10-27 11:58:14 +0600

    Form using Bootstrap

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+const Form = () => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          alert("clicked");
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default Form;
---
+++ tester.js
@@ -0,0 +1,6 @@
+const arr=['a', 'b', 'c', 'd', 'e'];
+const newArr = arr.map((value)=>{
+    return value.toUpperCase();
+})
+
+console.log(newArr); // ['A', 'B', 'C', 'D', 'E']
